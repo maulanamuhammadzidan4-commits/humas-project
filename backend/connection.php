ad50d0a2c73553a6cb3d705054c7b8a4f3a2a878
kayla <kaylanurlela7@gmail.com>
2026-09-14 11:07:35 +0700

    feat: add function for take data in connection.php

--- backend/connection.php
+++ backend/connection.php
@@ -11,3 +11,23 @@ try {
     echo "Koneksi database gagal: " . $e->getMessage();
     exit();
 }
+
+function show_all(string $table){
+    global $koneksi;
+    $query = "SELECT * FROM `$table`";
+    $raw_result = mysqli_query($koneksi, $query);
+    $result = $raw_result ? mysqli_fetch_all($raw_result, MYSQLI_ASSOC) : [];
+    mysqli_free_result($raw_result);
+
+    return $result;
+}
+
+function show_where(string $table, string $where){
+    global $koneksi;
+    $query = "SELECT * FROM `$table` WHERE `$where`";
+    $raw_result = mysqli_query($koneksi, $query);
+    $result = $raw_result ? mysqli_fetch_all($raw_result, MYSQLI_ASSOC) : [];
+    mysqli_free_result($raw_result);
+
+    return $result;
+}
